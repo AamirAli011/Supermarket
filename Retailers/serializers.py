87f6ca4958ed62ca34e9f0a40d693245a0128141
aamir <aali59398@gmail.com>
2023-04-09 00:04:00 +0500

    Updated formatting

--- Retailers/serializers.py
+++ Retailers/serializers.py
@@ -1,21 +1,25 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Product, Retailer, Customer
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
 
+
 class RetailerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Retailer
         fields = "__all__"
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
